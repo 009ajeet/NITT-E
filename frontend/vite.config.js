893ffbbc8b2e1f663bec0b9792693fdb03e1be2e
Ajeet Singh <as7641012@gmail.com>
2025-05-07 14:12:37 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    preserveSymlinks: true,
+    caseSensitive: true
+  },
 })
